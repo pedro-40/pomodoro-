6b0e2c4ceabbc8aabe10cb4e05480beaaa0ff763
pedro <vakal9895@gmail.com>
2025-09-20 01:22:14 +0330

    add coments

--- banner.py
+++ banner.py
@@ -4,4 +4,6 @@ except:
     print ("Please install colorama library")
 
 def banner() :
+    '''For start'''
+
     print(Fore.CYAN+"Pomodoro begins .(Good luck :)"+Fore.RESET)
--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -2,7 +2,7 @@ import time
 from colorama import Fore
 
 def work_time () :
-    ''' '''
+    '''Work time calculation'''
 
     pomodoro_time = 25
     while pomodoro_time != 0 :
@@ -13,12 +13,11 @@ def work_time () :
 
 
 def free_time():
+    '''Rest time calculation'''
 
     rest = 5
     while rest != 0 :
         print(Fore.YELLOW+f"Remaining rest time : {rest}"+Fore.RESET)
         rest -= 1
         time.sleep(60)
-
-
-work_time()
+    return rest
